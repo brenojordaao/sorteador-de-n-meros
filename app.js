cc63e6d7aecd3d0f66b8d056b5c756790cb60d62
Breno Jordão <breno.vasc@hotmail.com>
2025-01-28 15:17:50 -0300

    parseInt quanitade

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function sortear() {
-let quantidade = document.getElementById('quantidade').value; 
+let quantidade = parseInt(document.getElementById('quantidade').value); 
 let de = parseInt(document.getElementById('de').value); 
 let ate = parseInt(document.getElementById('ate').value); 
 }
